7fecf2026bb75d0392c8e02fe047ca781703668b
chenyitao <cyitao@foxmail.com>
2023-04-29 12:58:12 +0800

    chore: 添加注释

--- main.js
+++ main.js
@@ -4,33 +4,44 @@ const path = require("path")
 // const path = require("path")
 function createWindow() {
   const win = new BrowserWindow({
+    // 窗口的宽高
     width: 800,
     height: 600,
     webPreferences: {
+      // 预加载js代码，用于注册ipc事件，与主进程通信。
       preload: path.join(__dirname, "src/preload.js"),
     },
   })
   win.webContents.openDevTools()
 
+  // 以文件协议加载html文件，也就是 file://xxx/yyy/index.html
   win.loadFile("src/index.html")
   return win
 }
 
+// 当app初始化完成的时候
 app.whenReady().then(() => {
+  // 注册ipc事件
   ipcMain.on("read-file", (event, filePath) => {
     console.log("main filePath", filePath)
     const files = require("fs").readdirSync(filePath)
     console.log("main files", files)
-    win.webContents.send("file-list", files, "xjkljl")
+    // 发送文件夹读取结果
+    win.webContents.send(
+      /*事件的名字*/ "file-list",
+      /*参数1*/ files,
+      /*参数2*/ "xjkljl",
+    )
   })
 
+  // 创建窗口
   const win = createWindow()
   // on/off
-  app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+  // app.on("activate", () => {
+  //   if (BrowserWindow.getAllWindows().length === 0) {
+  //     createWindow()
+  //   }
+  // })
 })
 
 app.on("window-all-closed", () => {
--- src/preload.js
+++ src/preload.js
@@ -4,9 +4,12 @@ const { contextBridge } = require("electron")
 const ipcAPI = {
   readDir: () => {
     return new Promise((resolve) => {
+      // 准备好接受主进程传来的文件列表
       electron.ipcRenderer.on("file-list", (event, files, str) => {
+        // 返回给方法的调用者
         resolve(files)
       })
+      // 触发读取文件事件
       electron.ipcRenderer.send(
         "read-file",
         // 改成你要读取的目录
@@ -17,4 +20,5 @@ const ipcAPI = {
   },
 }
 
+// 往渲染进程的window对象上注入一些api，让渲染进程可以调用主进程的能力。
 contextBridge.exposeInMainWorld("ipcAPI", ipcAPI)
